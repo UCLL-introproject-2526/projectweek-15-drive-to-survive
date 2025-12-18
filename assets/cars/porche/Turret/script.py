4d3cfa2fa17aaac5ab20b44fc30767613b850775
Killianvb <killianvb111@gmail.com>
2025-12-18 11:51:56 +0100

    Porsche turret fixed

--- assets/cars/porche/Turret/script.py
+++ assets/cars/porche/Turret/script.py
@@ -20,9 +20,10 @@ class TurretUpgrade:
         # Shoot when the shoot key is pressed (use car's control settings)
         shoot_key = self.car.controls.get('shoot', pygame.K_e)
         if keys[shoot_key] and self.cooldown == 0 and self.ammo > 0:
-            self.shoot(zombies)
-            self.cooldown = self.max_cooldown
-            self.ammo -= 1
+            shot_fired = self.shoot(zombies)
+            if shot_fired:
+                self.cooldown = self.max_cooldown
+                self.ammo -= 1
             
         # Update bullets
         for bullet in self.bullets[:]:
@@ -94,7 +95,7 @@ class TurretUpgrade:
                         
     def shoot(self, zombies):
         if not zombies:
-            return
+            return False
             
         # Find nearest zombie in front of car
         nearest_zombie = None
@@ -125,6 +126,8 @@ class TurretUpgrade:
                 'dx': dx/length * self.bullet_speed,
                 'dy': dy/length * self.bullet_speed
             })
+            return True
+        return False
             
     def draw(self, cam_x):
         # Draw bullets
